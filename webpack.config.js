5e85b32071998cae4165e5371e13c5ee56aa8375
Shivaji Arjun Kakad <kakadshivaji70@gmail.com>
2019-04-06 22:54:00 +0530

    First Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require("webpack");
+var path = require("path");
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+var config = {
+entry: SRC_DIR + "/app/index.js",
+output:{
+    path : DIST_DIR + "/app",
+    filename: "bundle.js",
+    publicPath: "/app/"
+},
+module: {
+    rules:[{      // loaders is deprecated  | rules came in webpack 4
+        test: /\.js?/,
+        include: SRC_DIR,
+        loader: "babel-loader",
+        query:{
+            presets : ["@babel/preset-react"]
+        }
+    }]
+}
+};
+
+module.exports = config;
