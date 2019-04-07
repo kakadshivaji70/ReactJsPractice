43bd87ee37c46831eefa4a5e31d0b475a61c51df
Shivaji Arjun Kakad <kakadshivaji70@gmail.com>
2019-04-07 13:02:00 +0530

    first component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,12 @@
-console.log("Worked!");
+import React  from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+    render(){
+        return (
+            <div> Hello World!</div>
+        );
+    }
+}
+render(<App/>, window.document.getElementById("app"));
+
